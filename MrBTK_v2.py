aa7cf029a1f75eace1a0b9ed70613ac370b78602
DLBPointon <damonlbp@hotmail.co.uk>
2021-08-10 16:27:22 +0100

    remove dev string again

--- MrBTK_v2.py
+++ MrBTK_v2.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 
 def dotloader():
-    load_dotenv() #for testing
+    # load_dotenv() #for testing
     jira_user = os.getenv('JIRA_USER')
     jira_pass = os.getenv('JIRA_PASS')
     test_hook = os.getenv('SLACK_TEST')
