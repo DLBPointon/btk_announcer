168e32936c07200b6dba8cc62af41ce2b9270246
DLBPointon <damonlbp@hotmail.co.uk>
2021-11-25 14:16:07 +0000

    Added tracking for re-runs, should re-write the script in the future!

--- MrBTK_v2.py
+++ MrBTK_v2.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 
 def dotloader():
-    # load_dotenv() #for testing
+    load_dotenv() #for testing
     jira_user = os.getenv('JIRA_USER')
     jira_pass = os.getenv('JIRA_PASS')
     test_hook = os.getenv('SLACK_TEST')
@@ -43,6 +43,9 @@ def comment_check(auth_jira, projects):
     done = []
     running = []
     analysis = []
+    rerun = []
+    redone = []
+    reanalysis = []
 
     for issue in projects:
         btk_issues = auth_jira.issue(f'{issue}')
@@ -62,35 +65,60 @@ def comment_check(auth_jira, projects):
             if 'BTK ANALYSIS DONE' in comment.body or 'btk analysis done' in comment.body:
                 analysis.append(str(issue))
 
-    analysis_1, done_1, running_2, request_3 = list_setter(analysis, done, running, request)
+            if "need a new btk" in comment.body or "NEEDS A NEW BTK" in comment.body:
+                rerun.append(str(issue))
 
-    return done_1, running_2, request_3, analysis_1
+            if 'BTK REDONE' in comment.body or 'btk redone' in comment.body:
+                redone.append(str(issue))
+
+            if 'BTK ANALYSIS REDONE' in comment.body or 'btk analysis redone' in comment.body:
+                reanalysis.append(str(issue))
 
 
-def list_setter(analysis, done, running, request):
+
+    analysis_1, done_1, running_2, request_3, rerun_2, done_again, reanalysis_2 = list_setter(analysis, done, running,
+                                                                               request, rerun, redone, reanalysis)
+
+    return done_1, running_2, request_3, analysis_1, rerun_2, done_again, reanalysis_2
+
+
+def list_setter(analysis, done, running, request, rerun, redone, reanalysis):
     # Creates lists of unique issues
     done2 = list(set(done))
     done = list(set(done)-set(analysis))
     running = list(set(running))
     request = list(set(request))
 
+    rerun = list(set(rerun))
+    redone = list(set(redone))
+    rerun_2 = list(set(rerun) - set(redone))
+    reanalysis_2 = list(set(redone) - set(reanalysis))
+
+
+    print(f'{rerun} \n')
+    print(f"{redone}\n")
+    print(f"{rerun_2}\n")
+    print(f"{reanalysis_2}")
+
+
     # Removes any issues found in DONE
     request = list(set(request) - set(done2))
     running = list(set(running) - set(done2))
     analysis = list(set(analysis))
 
+
     # Removes any issues found in RUNNING
     request = list(set(request) - set(running))
 
-    return analysis, done, running, request
+    return analysis, done, running, request, rerun_2, redone, reanalysis_2
 
 
-def list_2_output(decon, curation, rapid, analysis):
+def list_2_output(decon, curation, rapid, analysis, rerun):
 
     req_start = f' --- Status == REQUESTS --- ALL CHANNELS ---'
     run_start = f' --- Status == RUNNING --- ALL CHANNELS ---'
     don_start = f' --- Status == NEED ANALYSIS --- ALL CHANNELS ---'
-    analysis_start = f' --- Status == ANALYSIS DONE --- ALL CHANNELS ---'
+    reruns_start = f' --- Status == RE-RUNS --- ALL CHANNELS ---'
 
     req_list = ''
     for i in decon[2], curation[2], rapid[2]:
@@ -112,7 +140,7 @@ def list_2_output(decon, curation, rapid, analysis):
             i = 'Queue Empty :+1:'
 
         don_list = don_list + f' {i}' + '\n'
-    
+
     warnings = ''
     if don_list.count('R') >= 10:
         warnings = warnings + 'Please Add {BTK ANALYSIS DONE} to finished tickets' + '\n'
@@ -135,7 +163,7 @@ def list_2_output(decon, curation, rapid, analysis):
 
     master_out = '{"text":"\n' + \
                  f' -------- MrBTK Report for {date.today()} START--------\n' + \
-                 f' --- Version 2.3 ---\n' + \
+                 f' --- Version 2.4 ---\n' + \
                  f' --- Organised Decon, Curation, Rapid --- \n' + \
                  f' *Curation = Everything including and post-curation*\n' + \
                  f'===================================================\n' + \
@@ -146,6 +174,9 @@ def list_2_output(decon, curation, rapid, analysis):
                  f'{don_start}\n' + \
                  f'{don_list}\n' + \
                  f'{warnings}\n' + \
+                 f'===================================================\n' + \
+                 f'Re-Run Request: {rerun[0]},{rerun[2]},{rerun[4]}\n' + \
+                 f'Re-Runs for Analysis: {rerun[6]},{rerun[7]},{rerun[8]}\n' + \
                  f'===================================================\n' + \
                  f'BTKed and in the Pipeline:  {counter}\n' + \
                  f'===================================================\n' + \
@@ -158,7 +189,8 @@ def list_2_output(decon, curation, rapid, analysis):
 def post_it(json, hook):
     #json = '{"text":"Why am i not working :face_with_monocle"}'
     webhook = f'{hook}'
-    os.popen(f"curl -X POST -H 'Content-type: application/json' --data '{json}' {webhook}").read()
+    #os.popen(f"curl -X POST -H 'Content-type: application/json' --data '{json}' {webhook}").read()
+    print(json)
 
 
 def main():
@@ -171,9 +203,9 @@ def main():
     decon_or_curation = [True, False]
 
     decon_analysis, curation_analysis, rapid_analysis = None, None, None
-    decon_done, decon_running, decon_request = None, None, None
-    curation_done, curation_running, curation_request = None, None, None
-    rapid_done, rapid_running, rapid_request = None, None, None
+    decon_done, decon_running, decon_request, decon_re, decon_redone, decon_analysis2 = None, None, None, None, None, None
+    curation_done, curation_running, curation_request, curation_re, curation_redone, curation_analysis2 = None, None, None, None, None, None
+    rapid_done, rapid_running, rapid_request, rapid_re, rapid_redone, rapid_analysis2 = None, None, None, None, None, None
 
     for i in project:
 
@@ -181,25 +213,32 @@ def main():
             for ii in decon_or_curation:
                 projects = labelled_btk(auth_jira, i, ii)
                 if ii:
-                    decon_done, decon_running, decon_request, decon_analysis = comment_check(auth_jira, projects)
+                    decon_done, decon_running, decon_request,\
+                    decon_analysis, decon_re, decon_redone,\
+                    decon_analysis2 = comment_check(auth_jira, projects)
                     # Above run list_setter
                 else:
-                    curation_done, curation_running, curation_request, curation_analysis = comment_check(auth_jira,
-                                                                                                         projects)
+                    curation_done, curation_running, curation_request,\
+                    curation_analysis, curation_re, curation_redone,\
+                    curation_analysis2 = comment_check(auth_jira,projects)
                     # Above run list_setter
 
         else:
             projects = labelled_btk(auth_jira, i, False)
 
-            rapid_done, rapid_running, rapid_request, rapid_analysis = comment_check(auth_jira, projects)
+            rapid_done, rapid_running, rapid_request,\
+            rapid_analysis, rapid_re, rapid_redone,\
+            rapid_analysis2 = comment_check(auth_jira, projects)
             # Above run list_setter
 
-    analysis = [decon_analysis, curation_analysis, rapid_analysis]
-    decon = [decon_done, decon_running, decon_request]
-    curation = [curation_done, curation_running, curation_request]
-    rapid = [rapid_done, rapid_running, rapid_request]
+    analysis = [decon_analysis, curation_analysis, rapid_analysis, ]
+    decon = [decon_done, decon_running, decon_request, ]
+    curation = [curation_done, curation_running, curation_request, ]
+    rapid = [rapid_done, rapid_running, rapid_request, ]
+    rerun = [decon_re, decon_redone, curation_re, curation_redone, rapid_re, rapid_redone,
+             decon_analysis2, curation_analysis2, rapid_analysis2]
 
-    master_out = list_2_output(decon, curation, rapid, analysis)
+    master_out = list_2_output(decon, curation, rapid, analysis, rerun)
     print(master_out)
 
     post_it(master_out, prod_hook)
